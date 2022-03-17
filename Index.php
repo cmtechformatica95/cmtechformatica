b315d6131c99fd802953f6d1ce787b0653dd1edd
Krossuk1 <101809502+Krossuk1@users.noreply.github.com>
2022-03-17 15:49:51 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD>
+<TITLE>Colegio Claudio Gay</TITLE>
+<LINK REL=StyleSheet HREF="Estilos/Styles.css" TYPE="text/css" MEDIA=screen>
+</HEAD>
+<BODY>
+<div class="banner_menu">
+<a href="Paginas/Pag1.php">Inicio</a>
+<a href="Paginas/Pag1.php">Reglamentos</a>
+<a href="Paginas/Pag1.php">Organigrama</a>
+<a href="Paginas/Pag1.php">Excelencia</a>
+<a href="Paginas/Pag1.php">Docs MINEDUC</a>
+<div class="banner_logo">
+</div>
+</BODY>
+</HTML>
